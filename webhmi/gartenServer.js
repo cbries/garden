bf989b21c8472ff26faded9d413a92798f06c3bf
root <root@raspberrypi.(none)>
2016-10-13 17:06:44 +0200

    gzip communication

--- webhmi/gartenServer.js
+++ webhmi/gartenServer.js
@@ -85,8 +85,29 @@ process.on('SIGINT', function() { console.log("SIGTERM : shutting down softly...
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+    //response.writeHead(404);
+    //response.end();
+
+	var raw = fs.createReadStream('index.html');
+	var acceptEncoding = request.headers['accept-encoding'];
+	if (!acceptEncoding) {
+		acceptEncoding = '';
+	}
+
+	// Note: this is not a conformant accept-encoding parser.
+	// See http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
+	if (acceptEncoding.match(/\bdeflate\b/)) {
+		response.writeHead(200, { 'content-encoding': 'deflate' });
+		raw.pipe(zlib.createDeflate()).pipe(response);
+	} else if (acceptEncoding.match(/\bgzip\b/)) {
+		response.writeHead(200, { 'content-encoding': 'gzip' });
+		raw.pipe(zlib.createGzip()).pipe(response);
+	} else {
+		response.writeHead(200, {});
+		raw.pipe(response);
+	}
+	
+	response.end();
 });
 
 function execLog(error, stdout, stderr) {
